f1923f6ec07dd54bf0afbb6b0045faed2d7662ba
izardml <nizartokuzet@gmail.com>
2023-05-20 14:47:46 +0700

    first commit

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)) {
+            return response([
+                'success' => false,
+                'message' => ['These credentials do not match our records.']
+            ], 404);
+        }
+
+        $token = $user->createToken('ApiToken')->plainTextToken;
+
+        $response = [
+            'success' => true,
+            'user' => $user,
+            'token' => $token,
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json([
+            'success' => true,
+        ], 200);
+    }
+}
